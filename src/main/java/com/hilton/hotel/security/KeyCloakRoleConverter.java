5234828d2021d8e2d4a5b19e0f6db7fd9a4a786d
Sanjana Adithya <sanjanaadithya2003@gmail.com>
2026-07-08 15:33:31 +0530

    KAN-10 | Implemented oauth2 security with keycloak

---
+++ src/main/java/com/hilton/hotel/security/KeyCloakRoleConverter.java
@@ -0,0 +1,33 @@
+package com.hilton.hotel.security;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class KeyCloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
+
+    @Override
+    public Collection<GrantedAuthority> convert(Jwt jwt) {
+        Map<String, Object> realmAccess =
+                (Map<String, Object>) jwt.getClaims().get("realm access");
+
+        if(realmAccess == null || !realmAccess.containsKey("roles")){
+            return List.of();
+
+        }
+        List<String> roles = (List<String>) realmAccess.get("roles");
+
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE "+ role))
+                .collect(Collectors.toList());
+    }
+
+}
+
